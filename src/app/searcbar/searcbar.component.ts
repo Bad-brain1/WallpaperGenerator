a119c49672941ba7d03af1f828bdbbe40b26f332
Bad-brain1 <radynov2014@yandex.ru>
2020-11-04 19:15:57 +0300

    add angular version (not ready)

---
+++ src/app/searcbar/searcbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searcbar',
+  templateUrl: './searcbar.component.html',
+  styleUrls: ['./searcbar.component.css']
+})
+export class SearcbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
